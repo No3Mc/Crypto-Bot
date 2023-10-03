1c7a438056abc6e8ebdd45f36a5072b7472aad0f
thr33 <41834061+No3Mc@users.noreply.github.com>
2023-10-03 23:13:19 +0100

    We crusing slowly

---
+++ core.py
@@ -0,0 +1,37 @@
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+df = pd.read_excel("eth.xlsx", engine="openpyxl", sheet_name="ETHUSD")
+
+df = df[pd.to_datetime(df['Date'], format='%m/%d/%Y', errors='coerce').notna()]
+
+df['Date'] = pd.to_datetime(df['Date'], format='%m/%d/%Y')
+
+df.set_index('Date', inplace=True)
+
+
+df_2020 = df[df.index.year == 2020]
+df_2021 = df[df.index.year == 2021]
+
+# test
+
+print(df_2020.columns) 
+print(df_2020.head())   
+
+if 'Close' in df_2020.columns:
+
+    X = df_2020[['Close']].values
+    y = df_2021[['Close']].values
+
+
+    model = LinearRegression()
+
+
+    model.fit(X, y)
+
+
+    price_2021_prediction = model.predict([[df_2020.iloc[-1]['Close']]])
+
+    print(f"Predicted price for 2021: {price_2021_prediction[0][0]:.2f}")
+else:
+    print("The 'Close' column does not exist in df_2020.")
