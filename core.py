5c77a5ef0b4582ce51af87fc8e861f8dfa0ad27a
thr33 <41834061+No3Mc@users.noreply.github.com>
2023-10-03 23:28:48 +0100

    Test works fine

--- core.py
+++ core.py
@@ -3,34 +3,26 @@ from sklearn.linear_model import LinearRegression
 
 df = pd.read_excel("eth.xlsx", engine="openpyxl", sheet_name="ETHUSD")
 
-df = df[pd.to_datetime(df['Date'], format='%m/%d/%Y', errors='coerce').notna()]
+df['Date'] = pd.to_datetime(df['Date'], format='%m/%d/%Y', errors='coerce')
 
-df['Date'] = pd.to_datetime(df['Date'], format='%m/%d/%Y')
+df.dropna(subset=['Date'], inplace=True)
 
 df.set_index('Date', inplace=True)
-
 
 df_2020 = df[df.index.year == 2020]
 df_2021 = df[df.index.year == 2021]
 
-# test
-
-print(df_2020.columns) 
-print(df_2020.head())   
-
 if 'Close' in df_2020.columns:
-
     X = df_2020[['Close']].values
     y = df_2021[['Close']].values
 
-
     model = LinearRegression()
-
 
     model.fit(X, y)
 
 
-    price_2021_prediction = model.predict([[df_2020.iloc[-1]['Close']]])
+    last_2020_price = df_2020.iloc[-1]['Close']
+    price_2021_prediction = model.predict([[last_2020_price]])
 
     print(f"Predicted price for 2021: {price_2021_prediction[0][0]:.2f}")
 else:
