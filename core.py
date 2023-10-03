48a7f0ee0a30012ebff0b5ed2265df71906fc75c
thr33 <41834061+No3Mc@users.noreply.github.com>
2023-10-04 00:57:36 +0100

    It reads the data

--- core.py
+++ core.py
@@ -1,29 +1,32 @@
 import pandas as pd
-from sklearn.linear_model import LinearRegression
-
-df = pd.read_excel("eth.xlsx", engine="openpyxl", sheet_name="ETHUSD")
-
-df['Date'] = pd.to_datetime(df['Date'], format='%m/%d/%Y', errors='coerce')
-
-df.dropna(subset=['Date'], inplace=True)
-
-df.set_index('Date', inplace=True)
-
-df_2020 = df[df.index.year == 2020]
-df_2021 = df[df.index.year == 2021]
-
-if 'Close' in df_2020.columns:
-    X = df_2020[['Close']].values
-    y = df_2021[['Close']].values
-
-    model = LinearRegression()
-
-    model.fit(X, y)
 
 
-    last_2020_price = df_2020.iloc[-1]['Close']
-    price_2021_prediction = model.predict([[last_2020_price]])
+excel_file_path = 'eth.xlsx'
+df_2020 = pd.read_excel(excel_file_path, header=None).T
 
-    print(f"Predicted price for 2021: {price_2021_prediction[0][0]:.2f}")
-else:
-    print("The 'Close' column does not exist in df_2020.")
+
+df_2020.columns = df_2020.iloc[0]
+
+df_2020 = df_2020.iloc[1:]
+
+
+df_2020.columns = ['Date', 'Open', 'High', 'Low', 'Close']
+
+
+df_2020['Date'] = pd.to_datetime(df_2020['Date'], format='%m/%d/%Y')
+
+
+training_start_date = '2020-01-01'
+training_end_date = '2021-01-01'
+testing_start_date = '2021-01-02'
+testing_end_date = '2022-01-01'
+
+training_data = df_2020[(df_2020['Date'] >= training_start_date) & (df_2020['Date'] <= training_end_date)]
+testing_data = df_2020[(df_2020['Date'] >= testing_start_date) & (df_2020['Date'] <= testing_end_date)]
+
+
+print("Training Data:")
+print(training_data.head())
+
+print("\nTesting Data:")
+print(testing_data.head())
--- test.py
+++ test.py
@@ -1,8 +1,5 @@
 import pandas as pd
-
 excel_file_path = 'eth.xlsx'
-
 df_2020 = pd.read_excel(excel_file_path, header=0)
-
 print(df_2020.head())
 
