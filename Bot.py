5678c2549625738bf72abc8ae61c30d78c0ade72
SevenB1 <qambarmadad12@gmail.com>
2023-10-07 00:14:42 +0500

    Mean Absolute Error and R-Squared

--- Bot.py
+++ Bot.py
@@ -1,22 +1,31 @@
-#import seaborn as sns
 import pandas as pd
-import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
 
-
-csv_file = ("Ethereum History(2019-2023).csv")
+# Load data
+csv_file = "Updated-Ethereum History(2019-2023) - Ethereum HIstory(2019-2023).csv"
 data = pd.read_csv(csv_file)
-train = data.drop(["Date"], axis = 1)
-test = data[["Open", "High"]]
-data['Open'] = data['Open'].str.replace(',', '').astype(float)
-data['High'] = data['High'].str.replace(',', '').astype(float)
-data['Low'] = data['Low'].str.replace(',', '').astype(float)
-data['Close'] = data['Close'].str.replace(',', '').astype(float)
 
-X_train, X_test, y_train, y_test = train_test_split(train, test, test_size=0.3, random_state=2)
+# Define features (X) and target (y)
+X = data[["Open", "Close", "High", "Low"]]
+y = data["Close"]  # You can choose "Close" as the target for prediction
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=2)
+
+# Initialize and train the linear regression model
 regr = LinearRegression()
 regr.fit(X_train, y_train)
-pred = regr.predict(X_test)
-pred
-regr.score
+
+# Make predictions on the test set
+y_pred = regr.predict(X_test)
+
+# Calculate Mean Absolute Error (MAE) as an evaluation metric
+mae = mean_absolute_error(y_test, y_pred)
+print("Mean Absolute Error:", mae)
+
+# Optionally, you can also calculate R-squared (coefficient of determination)
+r_squared = regr.score(X_test, y_test)
+print("R-squared:", r_squared)
+
--- test.py
+++ test.py
@@ -1,1 +1,31 @@
-import sklearn
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error
+
+# Load data
+csv_file = "Updated-Ethereum History(2019-2023) - Ethereum HIstory(2019-2023).csv"
+data = pd.read_csv(csv_file)
+
+# Define features (X) and target (y)
+X = data[["Open", "Close", "High", "Low"]]
+y = data["Close"]  # You can choose "Close" as the target for prediction
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=2)
+
+# Initialize and train the linear regression model
+regr = LinearRegression()
+regr.fit(X_train, y_train)
+
+# Make predictions on the test set
+y_pred = regr.predict(X_test)
+
+# Calculate Mean Absolute Error (MAE) as an evaluation metric
+mae = mean_absolute_error(y_test, y_pred)
+print("Mean Absolute Error:", mae)
+
+# Optionally, you can also calculate R-squared (coefficient of determination)
+r_squared = regr.score(X_test, y_test)
+print("R-squared:", r_squared)
+
