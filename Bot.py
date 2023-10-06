5d5b50030aa355dc4826bd987d73a8d63292b1aa
SevenB1 <qambarmadad12@gmail.com>
2023-10-06 08:52:35 +0500

    Update.

--- Bot.py
+++ Bot.py
@@ -1,7 +1,8 @@
 import seaborn as sns
 import pandas as pd
 import numpy as np
-
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 
 
 csv_file = ("Ethereum_History(2019-2020).csv")
