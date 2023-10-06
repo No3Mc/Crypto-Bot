2022c5bb999099a6150ea19a3bc874aa7965dcce
SevenB1 <qambarmadad12@gmail.com>
2023-10-07 01:26:46 +0500

    Update.

--- test.py
+++ test.py
@@ -1,31 +1,41 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_absolute_error
 
 # Load data
 csv_file = "Updated-Ethereum History(2019-2023) - Ethereum HIstory(2019-2023).csv"
 data = pd.read_csv(csv_file)
 
-# Define features (X) and target (y)
-X = data[["Open", "Close", "High", "Low"]]
-y = data["Close"]  # You can choose "Close" as the target for prediction
+# Preprocess the data: Set "Date" as the index and ensure it's in datetime format
+data['Date'] = pd.to_datetime(data['Date'], format="%m/%d/%Y")
+data.set_index('Date', inplace=True)
 
-# Split the data into training and testing sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=2)
+# Define features (X) and target variable (y)
+X = data[["High", "Low", "Close"]]  # Features (excluding "Open" as it's the target variable)
+y = data["Open"]  # Target variable
 
-# Initialize and train the linear regression model
+# Initialize and train the linear regression model on the entire dataset
 regr = LinearRegression()
-regr.fit(X_train, y_train)
+regr.fit(X, y)
 
-# Make predictions on the test set
-y_pred = regr.predict(X_test)
+# Define a range of future dates for prediction (replace with your desired date range)
+future_dates = pd.date_range(start="01/01/2023", end="01/10/2023")  # Example date range
 
-# Calculate Mean Absolute Error (MAE) as an evaluation metric
-mae = mean_absolute_error(y_test, y_pred)
-print("Mean Absolute Error:", mae)
+# Make predictions for each future date
+predicted_open_prices = []
 
-# Optionally, you can also calculate R-squared (coefficient of determination)
-r_squared = regr.score(X_test, y_test)
-print("R-squared:", r_squared)
+for input_date in future_dates:
+    # Create a feature vector for prediction
+    input_features = X.loc[input_date][["High", "Low", "Close"]].values.reshape(1, -1)
 
+    # Use the trained model to predict the Ethereum open price for the specified future date
+    predicted_open_price = regr.predict(input_features)[0]
+    predicted_open_prices.append(predicted_open_price)
+
+# Create a DataFrame to store the predicted open prices with corresponding dates
+predicted_data = pd.DataFrame({
+    "Date": future_dates,
+    "Predicted_Open": predicted_open_prices
+})
+
+# Display the predicted open prices
+print(predicted_data)
