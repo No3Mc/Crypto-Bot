bdd267b7069d69ffe83833a69ea997a4faf91f54
SevenB1 <qambarmadad12@gmail.com>
2023-10-07 22:41:48 +0500

    Update.

--- Bot.py
+++ Bot.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_absolute_error
 
 # Load data
 csv_file = "Updated-Ethereum History(2019-2023) - Ethereum HIstory(2019-2023).csv"
@@ -9,34 +10,62 @@ data = pd.read_csv(csv_file)
 data['Date'] = pd.to_datetime(data['Date'], format="%m/%d/%Y")
 data.set_index('Date', inplace=True)
 
+# Ensure the datetime index is sorted in ascending order (if not already)
+data.sort_index(inplace=True)
+
 # Define features (X) and target variable (y)
-X = data[["High", "Low", "Close"]]  # Features (excluding "Open" as it's the target variable)
-y = data["Open"]  # Target variable
+X = data[["High", "Low", "Open"]]  # Features (excluding "Close" as it's the target variable)
+y = data["Close"]  # Target variable
 
-# Initialize and train the linear regression model on the entire dataset
-regr = LinearRegression()
-regr.fit(X, y)
+# Check if the specified date range is valid
+start_date = "01/01/2019"
+end_date = "01/04/2023"
+if start_date not in data.index or end_date not in data.index:
+    print("Invalid date range. Please ensure the dates are present in the dataset.")
+else:
+    # Slice the dataset based on the specified date range
+    train_data = data.loc[start_date:end_date]
 
-# Define a range of future dates for prediction (replace with your desired date range)
-future_dates = pd.date_range(start="01/01/2023", end="01/10/2023")  # Example date range
+    # Initialize and train the linear regression model on the training data
+    regr = LinearRegression()
+    regr.fit(X.loc[train_data.index], y.loc[train_data.index])
 
-# Make predictions for each future date
-predicted_open_prices = []
+    # Define a range of future dates for prediction in "m/d/y" format
+    future_dates = pd.date_range(start="01/05/2023", end="01/10/2023")  # Example date range
 
-for input_date in future_dates:
-    # Create a feature vector for prediction
-    input_features = X.loc[input_date][["High", "Low", "Close"]].values.reshape(1, -1)
+    # Initialize a list for predicted close prices
+    predicted_close_prices = []
 
-    # Use the trained model to predict the Ethereum open price for the specified future date
-    predicted_open_price = regr.predict(input_features)[0]
-    predicted_open_prices.append(predicted_open_price)
+    for input_date in future_dates:
+        try:
+            # Create a feature vector for prediction
+            input_features = X.loc[input_date][["High", "Low", "Open"]].values.reshape(1, -1)
 
-# Create a DataFrame to store the predicted open prices with corresponding dates
-predicted_data = pd.DataFrame({
-    "Date": future_dates,
-    "Predicted_Open": predicted_open_prices
-})
+            # Use the trained model to predict the Ethereum close price for the specified future date
+            predicted_close_price = regr.predict(input_features)[0]
+            predicted_close_prices.append(predicted_close_price)
+        except KeyError:
+            # Handle the case where the date doesn't exist in the index
+            print(f"Warning: Data for date {input_date} not found. Using the previous date's prediction.")
+            if predicted_close_prices:
+                predicted_close_prices.append(predicted_close_prices[-1])
+            else:
+                print("Error: No previous predictions available.")
+                break
 
-# Display the predicted open prices
-print(predicted_data)
+    if predicted_close_prices:
+        # Create a DataFrame to store the predicted close prices with corresponding dates
+        predicted_data = pd.DataFrame({
+            "Date": future_dates,
+            "Predicted_Close": predicted_close_prices
+        })
+
+        # Display the predicted close prices
+        print(predicted_data)
+
+        # Evaluate the model's accuracy on the training data
+        y_true = y.loc[train_data.index]
+        y_pred = regr.predict(X.loc[train_data.index])
+        mae = mean_absolute_error(y_true, y_pred)
+        print("Mean Absolute Error on Training Set:", mae)
 
