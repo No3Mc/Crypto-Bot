0e872e4de6fb31b0947f3f68f045801bc120cf39
Qaisar7566 <qaisar.abbas@arrivy.com>
2023-10-07 21:58:57 +0500

    Set Trigger for Prediction

--- new.py
+++ new.py
@@ -8,3 +8,7 @@ from tensorflow.keras.layers import LSTM, Dense
 csv_file = "Ethereum History(2019-2023).csv"
 data = pd.read_csv(csv_file)
 data['Date'] = pd.to_datetime(data['Date'])
+
+# Used 'Close' prices for prediction
+closing_prices = data['Close'].values.reshape(-1, 1)
+
