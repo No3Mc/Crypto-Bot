33d0098bdd13483954ad1abe4e28eab1cccd6f23
Qaisar7566 <qaisar.abbas@arrivy.com>
2023-10-07 21:53:15 +0500

    Loaded Data

--- new.py
+++ new.py
@@ -3,3 +3,8 @@ import pandas as pd
 from sklearn.preprocessing import MinMaxScaler
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import LSTM, Dense
+
+# Load and preprocess data
+csv_file = "Ethereum History(2019-2023).csv"
+data = pd.read_csv(csv_file)
+data['Date'] = pd.to_datetime(data['Date'])
