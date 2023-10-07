eda2b4923686198bfefe0d0d9737a25ae7482e02
Qaisar7566 <qaisar.abbas@arrivy.com>
2023-10-07 22:06:19 +0500

    Converted time series to supervise problem

--- new.py
+++ new.py
@@ -16,3 +16,10 @@ closing_prices = data['Close'].values.reshape(-1, 1)
 scaler = MinMaxScaler()
 normalized_data = scaler.fit_transform(closing_prices)
 
+# Convert time series to supervised learning problem
+X, y = [], []
+for i in range(60, len(normalized_data) - 1):  # using last 60 days to predict next day
+    X.append(normalized_data[i-60:i, 0])
+    y.append(normalized_data[i, 0])
+X, y = np.array(X), np.array(y)
+
